cec6779ec3b356944d8b4633c966e1e329318e9e
ferafael <circuncisaofabio@icloud.com>
2021-07-13 15:28:01 -0300

    models do todo

--- bookdecozinha/bookcozinha/models.py
+++ bookdecozinha/bookcozinha/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Todo(models.Model):
+    todo = models.TextField(max_length= 150)
+    title_todo = models.CharField(max_length=60)
+    edit = models.BooleanField()
+    delete = models.BooleanField()
+    data = models.DateField(auto_now=True)
+
+
+
