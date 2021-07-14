b1035c8a9939db4b891aad71af47546f6146d4c2
ferafael <circuncisaofabio@icloud.com>
2021-07-14 14:11:39 -0300

    criar a classe list_receita tera o list_display com os fomres qeu estao vindo do model.

--- bookdecozinha/bookcozinha/admin.py
+++ bookdecozinha/bookcozinha/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from django.utils.html import format_html
 
 # Register your models here.
+from .models import cardapio
+
+
+class list_receita(admin.ModelAdmin):
+    list_display = ['nome_receita', 'ingrediente', 'modo_de_preparo' ,'informacoes_basicas']
+    search_receita = ['key']
+    list_per_page = 10
+
+
+admin.site.register(cardapio,list_receita)
+
+
